2bcb4e80e35ec3bdc338b154a79c089f409000c1
benki <tanashin@bj.wakwak.com>
2020-12-12 18:00:26 +0900

    add err! macro

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,27 @@ macro_rules! ok {
         match $val {
             Some(v) => Ok(v),
             None => Err($crate::anyhow!(
-                "[{}:{}] {} is none.",
+                "[{}:{}] {} is None.",
                 file!(),
                 line!(),
                 stringify!($val)
+            ))
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! err {
+    ($val:expr) => {
+        match $val {
+            Some(v) => Err($crate::anyhow!(
+                "[{}:{}] {} is Some({:?}).",
+                file!(),
+                line!(),
+                stringify!($val),
+                v
             )),
+            None => Ok(())
         }
     };
 }
@@ -23,11 +39,17 @@ mod tests {
     fn ok() {
         let a: Result<i32> = ok!(Some(1));
         assert!(a.is_ok());
+
+        let b = err!(Option::<i32>::None);
+        assert!(b.is_ok());
     }
 
     #[test]
     fn err() {
         let a: Result<i32> = ok!(None);
         assert!(a.is_err());
+
+        let b = err!(Some(1));
+        assert!(b.is_err());
     }
 }
